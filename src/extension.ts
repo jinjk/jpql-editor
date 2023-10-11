2af01c2747de9fa70a1ae3efac9b88cdf89eb7c7
jjin <jiankun.jin@gmail.com>
2023-10-11 17:52:09 +0800

    debug works for vscode engine v1.80.0

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,22 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import JpqlParser from './antlr4/JpqlParser';
+import { CharStreams, CommonTokenStream, RuleNode } from 'antlr4';
+import JpqlLexer from './antlr4/JpqlLexer';
+import JpqlVisitor from './antlr4/JpqlVisitor';
+
+class CustomVisitor extends JpqlVisitor<string | undefined> {
+	visitChildren(ctx: RuleNode) {
+	  if (!ctx) {
+		return;
+	  }
+	  else {
+		console.log(ctx.getText());
+		return super.visitChildren(ctx);
+	  }
+	}
+  }
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -16,6 +32,13 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('helloworld.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
+		let str = "select v.name from vendor v where v.id = 1";
+		let inputStream = CharStreams.fromString(str);
+		let lexer = new JpqlLexer(inputStream);
+		let tokenStream = new CommonTokenStream(lexer);
+		let parser = new JpqlParser(tokenStream);
+		let tree = parser.start();
+		tree.accept(new CustomVisitor());
 		vscode.window.showInformationMessage('Hello World from HelloWorld!');
 	});
 
